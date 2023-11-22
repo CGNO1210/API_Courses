da5b8d20f35656a0a0a42f151770886faa01dafc
NguyenHuuNgoc <cgno1210@gmail.com>
2023-11-22 23:30:17 +0700

    update config 2

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -11,6 +11,9 @@ import { Sequelize } from 'sequelize';
 // });
 
 // Option 3: Passing parameters separately (other dialects)
+
+
+
 const sequelize = new Sequelize('sern', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
